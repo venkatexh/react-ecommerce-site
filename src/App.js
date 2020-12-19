61a0989489b625f9f2b7125e2addc279c4439c39
Venkatesh <venky.ch59@gmail.com>
2020-12-19 08:41:53 +0000

    add shopping page with collections

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
 import Homepage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
-  return (
-    <div className="App">
-      <Homepage />
-    </div>
-  );
+	return (
+		<div>
+			<Switch>
+				<Route exact path='/' component={Homepage} />
+				<Route path='/shop' component={ShopPage} />
+			</Switch>
+		</div>
+	)
 }
-
 export default App;
