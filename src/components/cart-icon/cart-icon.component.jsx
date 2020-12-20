d555ed7e7a81247c344c965f7a43dc8e64b551e9
Venkatesh <venky.ch59@gmail.com>
2020-12-20 16:55:09 +0000

    implement cart toggle using redux

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import './cart-icon.styles.scss'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg' 
 
-const CartIcon = () => (
-	<div className='cart-icon'>
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
+import { connect } from 'react-redux'
+
+const CartIcon = ({ toggleCartHidden }) => (
+	<div className='cart-icon' onClick={toggleCartHidden}>
 		<ShoppingIcon className='shopping-icon' />
 		<span className='item-count'>0</span>
 	</div>
 )
 
-export default CartIcon
+const mapDispatchToProps = dispatch => ({
+	toggleCartHidden: () => dispatch(toggleCartHidden())
+})
+
+export default connect(null, mapDispatchToProps)(CartIcon)
