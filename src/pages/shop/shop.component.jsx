4e8fe84f68e0d5055b8cd80d5da99c1c9c17957a
Venkatesh Chakrabarty <venkatesh@chaosnative.com>
2021-11-15 14:57:39 +0530

    Add directory and shop to Redux
    Signed-off-by: Venkatesh Chakrabarty <venkatesh@chaosnative.com>

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,10 @@
-import React, { Component } from 'react'
-import SHOP_DATA from './shop.data.js'
+import React from 'react'
+import { connect} from "react-redux";
+import { createStructuredSelector} from "reselect";
 import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import { selectCollections} from "../../redux/shop/shop.selector";
 
-class ShopPage extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			collections: SHOP_DATA
-		}
-	}
-	
-	render() {
-		const { collections } = this.state
+const ShopPage = ( {collections }) => {{
 		return (
 			<div className="shop-page">
 			{ 
@@ -23,4 +16,8 @@ class ShopPage extends Component {
 		)
 	}
 }
-export default ShopPage
+
+const mapStateToProps = createStructuredSelector({
+	collections: selectCollections
+})
+export default connect(mapStateToProps)(ShopPage)
