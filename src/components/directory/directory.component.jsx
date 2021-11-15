305292b5fd6caf89891c98a0ecfb49d7adaa805d
Venkatesh Chakrabarty <venkatesh@chaosnative.com>
2021-11-15 16:59:25 +0530

    Update code
    Signed-off-by: Venkatesh Chakrabarty <venkatesh@chaosnative.com>

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -8,7 +8,7 @@ const Directory = ({sections}) => {
 		return (
 			<div className="directory-menu">
 				{
-					this.state.sections.map(({ title, imageUrl, id, size }) => (
+					sections.map(({ title, imageUrl, id, size }) => (
 					<MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
 					))
 				}
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,8 +1,27 @@
-import { createSelector} from "reselect";
+import { createSelector } from "reselect";
 
-const selectShop = state => state.shop;
+const selectShop = (state) => state.shop;
+
+const ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+};
 
 export const selectCollections = createSelector(
   [selectShop],
-  shop => shop.collections
-)
+  (shop) => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
+export const selectCollection = (collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+  );
